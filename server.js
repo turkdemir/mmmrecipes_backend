e0290c4a68ff67ffd4fc6339845d44963e0c132c
TafatsuM <turkdemirmustafa@gmail.com>
2021-07-28 17:03:16 +0300

    changed cors origin

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const dbConfig= require('./app/config/db.config')
 const app = express();
 
 var corsOptions = {
-  origin: "http://localhost:8081"
+  origin: "https://mmmrecipesfront.herokuapp.com/"
 };
 
 app.use(cors(corsOptions));
